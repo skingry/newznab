97b10ef83b927df981d14fec3b55c15a240fc4ee
Seth Kingry <sjkingry@gmail.com>
2013-05-09 18:25:01 -0700

    Moved and renamed the screen script

---
+++ misc/update_scripts/newznab.sh
@@ -0,0 +1,37 @@
+#!/usr/local/bin/bash
+
+source ~/.bash_profile
+
+# Define how long to sleep between runs
+SLEEP="60"
+
+# Might as well say the last time we optimized the Db was right now...
+LAST=`date +%s`
+
+while :
+
+do
+  # Grab the current time since epoch
+  CURRENT=`date +%s`
+
+  # Update binaries
+  php update_binaries.php
+
+  # Update releases
+  php update_releases.php
+
+  # Compare the saved epoch time with the epoch time of the last Db optimization
+  DIFF=$((${CURRENT}-${LAST}))
+
+  # If greater than one day, then do it!
+  if [ ${DIFF} -gt 86400 ]; then
+    LAST=`date +%s`
+    echo "Optimizing DB..."
+    php optimise_db.php
+  fi
+
+  # Stop. Hammertime.
+  echo "waiting ${SLEEP} seconds..."
+  sleep ${SLEEP}
+done
+
